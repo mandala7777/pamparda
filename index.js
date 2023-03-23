1b5d9f54aafec895362642cdd23ef203e75f082b
mandala7777 <105830381+mandala7777@users.noreply.github.com>
2023-03-23 14:22:08 +0700

    Update index.js
    added try catch block

--- index.js
+++ index.js
@@ -8,12 +8,16 @@ app.get('/', async (req, res) => {
     const browser = await puppeteer.launch({headless:true})
     const page = await browser.newPage()
 
-    await page.goto('https://google.com/')
-    await page.waitForSelector('#SIvCob')
+    try {
+        await page.goto('https://google.com/')
+        await page.waitForSelector('#SIvCob')
 
     const text = await page.$eval('#SIvCob', elem => elem.textContent)
     res.send(text)
     await browser.close()
+    } catch (error) {
+        console.log(error)
+    }
 
 })
 
