e364b2a49a0504a9aef70171c01c6a1f2ef25e64
mandala7777 <105830381+mandala7777@users.noreply.github.com>
2023-03-23 11:39:29 +0700

    Add files via upload
    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+
+const app=express()
+const PORT = process.env.PORT||5000
+
+app.get('/', async (req, res) => {
+    const browser = await puppeteer.launch({headless:true})
+    const page = await browser.newPage()
+
+    await page.goto('https://google.com/')
+    await page.waitForSelector('#SIvCob')
+
+    const text = await page.$eval('#SIvCob', elem => elem.textContent)
+    res.send(text)
+    await browser.close()
+
+})
+
+
+
+app.listen(PORT, () => {console.log(PORT)})
